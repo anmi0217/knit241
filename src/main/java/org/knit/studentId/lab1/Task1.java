30286c9666166e01ea0df6eb145d19afbc6014a6
alkogol <unknowngy@gmail.com>
2024-09-15 14:37:11 +0400

    Содание шаблона проекта для выполнения задач на языке Java

---
+++ src/main/java/org/knit/studentId/lab1/Task1.java
@@ -0,0 +1,14 @@
+package org.knit.studentId.lab1;
+
+/**
+ * Задача 0 Шаблон
+ * Вывести на экран числа от 1 до 100
+ *
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            System.out.println(i);
+        }
+    }
+}
