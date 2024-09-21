3ab000901731af25eb99bd66a018189840500eb1
sosnovskiim <darkisosn2805@gmail.com>
2024-09-21 20:31:45 +0400

    Задача 2

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -2,6 +2,6 @@ package org.knit;
 
 public class Main {
     public static void main(String[] args) {
-        new SimpleUrl("https://test.ru/test/1072/page.jsp?intParam=12345&doubleParam=3.14&textParameter=someText");
+        System.out.println(new SimpleUrl("https://test.ru/test/1072/page.jsp?intParam=12345&doubleParam=3.14&textParameter=someText"));
     }
 }
