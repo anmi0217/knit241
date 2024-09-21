5e4d04aeca854c1ddfe029d1e105e82177624340
sosnovskiima <mikhailsosnovskii2805@gmail.com>
2024-09-21 08:53:26 +0400

    Revert "Задача 1"
    This reverts commit 5683482eeac091f09860aba4dff0f7cb99a92823.

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -1,9 +1,7 @@
 package org.knit;
 
-import org.knit.tasks.task1.Task1;
-
 public class Main {
     public static void main(String[] args) {
-        new Task1().execute();
+        System.out.println("Hello world!");
     }
 }
