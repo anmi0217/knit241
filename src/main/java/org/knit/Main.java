e954803eafc7fb4dfd1df2d2ca6d071b44e48d84
sosnovskiim <darkisosn2805@gmail.com>
2024-09-22 12:49:22 +0400

    Задача 1

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -2,17 +2,6 @@ package org.knit;
 
 public class Main {
     public static void main(String[] args) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 1; i < 100; i += 10) {
-            for (int j = i; j < i + 10; j++) {
-                result.append(j);
-                if (j < i + 9) {
-                    result.append(",");
-                } else {
-                    result.append("\n");
-                }
-            }
-        }
-        System.out.println(result);
+        System.out.println("Hello world!");
     }
 }
