86f42b0ae954c789b335b3373bb9cdf10752c8cf
sosnovskiim <darkisosn2805@gmail.com>
2024-10-03 21:15:08 +0400

    Задача 4

---
+++ src/main/java/org/knit/tasks/task4/Cube.java
@@ -0,0 +1,14 @@
+package org.knit.tasks.task4;
+
+public class Cube extends Shape {
+    private final double edge;
+
+    public Cube(double edge) {
+        this.edge = edge;
+    }
+
+    @Override
+    public double getVolume() {
+        return Math.pow(edge, 3);
+    }
+}
