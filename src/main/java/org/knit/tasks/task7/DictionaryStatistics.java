57163d37ad18975a641e33ab49c18d40deedfed5
sosnovskiim <darkisosn2805@gmail.com>
2024-10-16 21:28:58 +0400

    Задача 7

--- src/main/java/org/knit/tasks/task7/DictionaryStatistics.java
+++ src/main/java/org/knit/tasks/task7/DictionaryStatistics.java
@@ -1,28 +1,82 @@
 package org.knit.tasks.task7;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class DictionaryStatistics {
     private final String[] dictionary;
-    // todo private int dictionarySize;
-    // todo private int palindromesNumber;
-    // todo private int maxWordLength;
-    // todo private int minWordLength;
+    private final int dictionarySize;
+    private final int palindromesNumber;
+    private final int maxWordLength;
+    private final int minWordLength;
     private final char[] alphabet;
-    // todo private int[] letterFrequency;
+    private final Map<Character, Integer> letterFrequency = new HashMap<>();
 
     public DictionaryStatistics(String[] dictionary, char[] alphabet) {
         this.dictionary = dictionary;
         this.alphabet = alphabet;
+
+        for (char letter : alphabet) {
+            letterFrequency.put(letter, 0);
+        }
+
+        int k = 0, max = 0, min = Integer.MAX_VALUE;
+        for (String word : dictionary) {
+            if (isPalindrome(word)) {
+                k++;
+            }
+            max = Integer.max(word.length(), max);
+            min = Integer.min(word.length(), min);
+            countLetterFrequency(word);
+        }
+
+        dictionarySize = dictionary.length;
+        palindromesNumber = k;
+        maxWordLength = max;
+        minWordLength = min;
     }
 
-    // todo public int getDictionarySize()
+    private boolean isPalindrome(String word) {
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-    // todo public int getPalindromesNumber()
+    private void countLetterFrequency(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            if (letterFrequency.containsKey(word.charAt(i))) {
+                letterFrequency.put(word.charAt(i), letterFrequency.get(word.charAt(i)) + 1);
+            }
+        }
+    }
 
-    // todo public int getMaxWordLength()
+    public int getDictionarySize() {
+        return dictionarySize;
+    }
 
-    // todo public int getMinWordLength()
+    public int getPalindromesNumber() {
+        return palindromesNumber;
+    }
 
-    // todo public String getRandomWord()
+    public int getMaxWordLength() {
+        return maxWordLength;
+    }
 
-    // todo public void printLetterStatistics()
+    public int getMinWordLength() {
+        return minWordLength;
+    }
+
+    public String getRandomWord() {
+        return dictionary[new Random().nextInt(dictionarySize)];
+    }
+
+    public void printLetterStatistics() {
+        for (char letter : alphabet) {
+            System.out.printf("%c - %d\n", letter, letterFrequency.get(letter));
+        }
+    }
 }
