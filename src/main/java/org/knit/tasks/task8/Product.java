5cbf39555a7cfe09ca886bd48fcfae435a293cf7
sosnovskiim <darkisosn2805@gmail.com>
2024-10-17 21:33:17 +0400

    Задача 8

---
+++ src/main/java/org/knit/tasks/task8/Product.java
@@ -0,0 +1,52 @@
+package org.knit.tasks.task8;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private final int mass;
+
+    public Product(String name, int price, int mass) {
+        this.name = name;
+        this.price = price;
+        this.mass = mass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getMass() {
+        return mass;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() * price * mass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return getName().equals(product.getName()) &&
+                getPrice() == product.getPrice() &&
+                getMass() == product.getMass();
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Product { name='%s', price=%d, mass=%d }",
+                name, price, mass
+        );
+    }
+}
