b486bcd82c7827937fc0e49ab84d36a92478be34
alkogol <unknowngy@gmail.com>
2024-10-25 12:53:11 +0400

    Lection № 4

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -1,12 +1,8 @@
 package org.knit;
 
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Random random = new Random();
-        int index = random.nextInt(4);
 
     }
 }
