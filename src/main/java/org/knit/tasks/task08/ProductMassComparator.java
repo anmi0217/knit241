a84e919326c29fc72f9cfeb4893011b0658f82a4
sosnovskiim <darkisosn2805@gmail.com>
2024-10-30 21:18:34 +0400

    Задача 8

---
+++ src/main/java/org/knit/tasks/task08/ProductMassComparator.java
@@ -0,0 +1,10 @@
+package org.knit.tasks.task08;
+
+import java.util.Comparator;
+
+public class ProductMassComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product p1, Product p2) {
+        return Integer.compare(p1.getMass(), p2.getMass());
+    }
+}
