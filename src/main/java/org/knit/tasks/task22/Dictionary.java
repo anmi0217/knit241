4357915084c28fd96bec859e874f898ac006961c
sosnovskiima <sosnovskiim@ya.ru>
2024-12-07 11:41:43 +0400

    Задачи 18-23

---
+++ src/main/java/org/knit/tasks/task22/Dictionary.java
@@ -0,0 +1,37 @@
+package org.knit.tasks.task22;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dictionary<K, V> {
+    private final List<Pair<K, V>> dictionary = new ArrayList<>();
+
+    public void put(K key, V value) {
+        dictionary.add(new Pair<>(key, value));
+    }
+
+    public V get(K key) {
+        for (Pair<K, V> pair : dictionary) {
+            if (pair.getKey().equals(key)) {
+                return pair.getValue();
+            }
+        }
+        System.out.println("Элемент с ключом " + key + " не найден");
+        return null;
+    }
+
+    public void remove(K key) {
+        for (Pair<K, V> pair : dictionary) {
+            if (pair.getKey().equals(key)) {
+                dictionary.remove(pair);
+                return;
+            }
+        }
+        System.out.println("Элемент с ключом " + key + " не найден");
+    }
+
+    @Override
+    public String toString() {
+        return dictionary.toString();
+    }
+}
