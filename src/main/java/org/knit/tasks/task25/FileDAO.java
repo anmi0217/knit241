47d032bd02d0cf61fa9bba5bf2a65b9b013fe441
sosnovskiim <darkisosn2805@gmail.com>
2024-12-20 05:43:55 +0400

    Задача 25

---
+++ src/main/java/org/knit/tasks/task25/FileDAO.java
@@ -0,0 +1,33 @@
+package org.knit.tasks.task25;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class FileDAO {
+    private static final String query = "INSERT INTO files (file_name, file_binary) VALUES (?, ?)";
+
+    public void saveFile(File file) throws SQLException, IOException {
+        try (
+                Connection connection = DatabaseConnection.getConnection();
+                PreparedStatement statement = connection.prepareStatement(query)
+        ) {
+            statement.setString(1, file.getName());
+            try (
+                    FileInputStream fileInputStream = new FileInputStream(file);
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()
+            ) {
+                byte[] buffer = new byte[(int) file.length()];
+                for (int length; (length = fileInputStream.read(buffer)) != -1; ) {
+                    byteArrayOutputStream.write(buffer, 0, length);
+                }
+                statement.setBytes(2, byteArrayOutputStream.toByteArray());  // Содержимое файла в формате BYTEA
+            }
+            statement.executeUpdate();
+        }
+    }
+}
---
+++ src/main/java/org/knit/tasks/task25/SimpleFileVisitor.java
@@ -0,0 +1,42 @@
+package org.knit.tasks.task25;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.sql.SQLException;
+
+public class SimpleFileVisitor implements FileVisitor<Path> {
+    private final FileDAO fileDAO;
+
+    public SimpleFileVisitor() {
+        this.fileDAO = new FileDAO();
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        try {
+            fileDAO.saveFile(file.toFile());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) {
+        System.out.println("Не удалось открыть файл или папку: " + file);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+        return FileVisitResult.CONTINUE;
+    }
+}
