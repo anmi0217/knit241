a2a819de8d7bdd2a94fe646160c8c2c7ed69e33b
sosnovskiim <darkisosn2805@gmail.com>
2024-12-20 04:46:58 +0400

    Задача 24

--- src/main/java/org/knit/tasks/task24/Task24.java
+++ src/main/java/org/knit/tasks/task24/Task24.java
@@ -1,16 +1,55 @@
 package org.knit.tasks.task24;
 
-import java.sql.*;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Task24 {
     public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\sosnovskiima\\AppData\\Roaming\\DBeaverData\\workspace6\\.metadata\\sample-database-sqlite-1\\Chinook.db");
-        PreparedStatement statement = connection.prepareStatement(
-                "SELECT * FROM Album");
-        statement.execute();
-        ResultSet set = statement.getResultSet();
-        while (set.next()) {
-            System.out.println(set.getString("Title"));
+        UserService userService = new UserService();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Доступные команды для управления пользователями:\n" +
+                "\tadd [name] [email] - добавляет нового пользователя;\n" +
+                "\tupdate [id] [new_name] [new_email] - обновляет данные пользователя;\n" +
+                "\tdelete [id] - удаляет пользователя по номеру;\n" +
+                "\tlist - выводит список всех пользователей;\n" +
+                "\texit - выход из программы.");
+        String action = "";
+        while (!action.equals("exit")) {
+            System.out.print("\nВведите команду: ");
+            String[] params = scanner.nextLine().split(" ");
+            action = params[0];
+            switch (action) {
+                case "add":
+                    if (params.length == 3) {
+                        System.out.println(userService.addUser(params[1], params[2]));
+                    } else {
+                        System.out.println("Неверное количество параметров.");
+                    }
+                    break;
+                case "update":
+                    if (params.length == 4) {
+                        System.out.println(userService.updateUser(Integer.parseInt(params[1]), params[2], params[3]));
+                    } else {
+                        System.out.println("Неверное количество параметров.");
+                    }
+                    break;
+                case "delete":
+                    if (params.length == 2) {
+                        System.out.println(userService.deleteUser(Integer.parseInt(params[1])));
+                    } else {
+                        System.out.println("Неверное количество параметров.");
+                    }
+                    break;
+                case "list":
+                    System.out.println("Список пользователей:");
+                    userService.getAllUsers().forEach(System.out::println);
+                    break;
+                case "exit":
+                    System.out.println("Выход из программы.");
+                    break;
+                default:
+                    System.out.println("Ошибка: команда \"" + action + "\" не найдена.");
+            }
         }
     }
 }
---
+++ src/main/java/org/knit/tasks/task24/UserService.java
@@ -0,0 +1,47 @@
+package org.knit.tasks.task24;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserService {
+    private final UserDAO userDAO;
+
+    public UserService() throws SQLException {
+        this.userDAO = new UserDAOImpl();
+    }
+
+    public String addUser(String name, String email) {
+        if (userDAO.isEmailNotExists(email)) {
+            userDAO.addUser(new User(name, email));
+            return "Пользователь " + email + " успешно добавлен.";
+        }
+        return "Пользователь " + email + " уже существует.";
+    }
+
+    public String updateUser(int id, String name, String email) {
+        User user = userDAO.getUserById(id);
+        if (user != null) {
+            if (userDAO.isEmailNotExists(email)) {
+                user.setName(name);
+                user.setEmail(email);
+                userDAO.updateUser(user);
+                return "Данные пользователя " + email + " успешно обновлены.";
+            }
+            return "Пользователь " + email + " уже существует.";
+        }
+        return "Пользователя с номером " + id + " не существует.";
+    }
+
+    public String deleteUser(int id) {
+        User user = userDAO.getUserById(id);
+        if (user != null) {
+            userDAO.deleteUser(id);
+            return "Пользователь " + user.getEmail() + " успешно удален.";
+        }
+        return "Пользователя с номером " + id + " не существует.";
+    }
+
+    public List<User> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+}
