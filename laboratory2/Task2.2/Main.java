29ac6aff8f5560cf08545d1fe0fe8f49037b877c
Veronika_Steklo <151031140+VeronikaSteklo@users.noreply.github.com>
2025-02-22 15:36:19 +0400

    Create Main.java

---
+++ laboratory2/Task2.2/Main.java
@@ -0,0 +1,22 @@
+package Semestr_2.Task2;
+
+// 5. Демонстрация использования
+public class Main {
+    public static void main(String[] args) {
+        Coffee coffee = new EspressoCoffee();
+        System.out.println(coffee.getDescription() + " | Цена: $" + coffee.getCost() + " | Калорийность: " + coffee.getCalories() + " ккал");
+
+        coffee = new SizeDecorator(coffee, "medium");
+        System.out.println(coffee.getDescription() + " | Цена: $" + coffee.getCost() + " | Калорийность: " + coffee.getCalories() + " ккал");
+
+        coffee = new Milk(coffee);
+        System.out.println(coffee.getDescription() + " | Цена: $" + coffee.getCost() + " | Калорийность: " + coffee.getCalories() + " ккал");
+
+        coffee = new Caramel(coffee);
+        System.out.println(coffee.getDescription() + " | Цена: $" + coffee.getCost() + " | Калорийность: " + coffee.getCalories() + " ккал");
+
+        coffee = new Chocolate(coffee);
+        System.out.println(coffee.getDescription() + " | Цена: $" + coffee.getCost() + " | Калорийность: " + coffee.getCalories() + " ккал");
+    }
+}
+
