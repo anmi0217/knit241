200ef7c044b308ec811cac34a3e3c1f56659b9f5
Veronika_Steklo <151031140+VeronikaSteklo@users.noreply.github.com>
2025-02-22 15:38:00 +0400

    Задание 2.3

---
+++ laboratory2/Task2.3/BusinessCar.java
@@ -0,0 +1,8 @@
+package Semestr_2.Task3;
+
+class BusinessCar implements Transport {
+    @Override
+    public String getSpecifications() {
+        return "Бизнес-седан: комфортный салон, кожаные сиденья";
+    }
+}
---
+++ laboratory2/Task2.3/FamilyVan.java
@@ -0,0 +1,8 @@
+package Semestr_2.Task3;
+
+class FamilyVan implements Transport {
+    @Override
+    public String getSpecifications() {
+        return "Семейный минивэн: перевозка 6+ пассажиров";
+    }
+}
