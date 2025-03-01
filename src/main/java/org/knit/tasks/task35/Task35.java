08bfa77c5e633023f660ae329052afba5c2cb9d2
sosnovskiima <mikhailsosnovskii2805@gmail.com>
2025-03-01 11:40:46 +0400

    Задачи 35

---
+++ src/main/java/org/knit/tasks/task35/Task35.java
@@ -0,0 +1,22 @@
+package org.knit.tasks.task35;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Task35 {
+    public static void main(String[] args) {
+        int runnersNumber = 5;
+        CyclicBarrier startBarrier = new CyclicBarrier(runnersNumber, () ->
+                System.out.println("Все бегуны готовы к старту. Гонка начинается.")
+        );
+        CyclicBarrier finishBarrier = new CyclicBarrier(runnersNumber, () ->
+                System.out.println("Гонка завершена.")
+        );
+        ExecutorService executorService = Executors.newFixedThreadPool(runnersNumber);
+        for (int i = 0; i < runnersNumber; i++) {
+            executorService.submit(new Runner(i, startBarrier, finishBarrier));
+        }
+        executorService.shutdown();
+    }
+}
