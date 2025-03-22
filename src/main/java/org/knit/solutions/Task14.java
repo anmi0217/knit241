2194193df14371b2b5450229f40d356bd334a3d0
heta76 <lenzhaev@gmail.com>
2025-03-22 11:31:25 +0400

    task14 начало работы

--- src/main/java/org/knit/solutions/Task14.java
+++ src/main/java/org/knit/solutions/Task14.java
@@ -1,4 +1,12 @@
 package org.knit.solutions;
 
-public class Task14 {
+import org.knit.TaskDescription;
+
+@TaskDescription(taskNumber = 14, taskDescription = "Краткое описание задачи 1", href = "org/knit/solutions/taskExampleClasses/README.md")
+public class Task14 implements Solution{
+
+    @Override
+    public void execute() {
+        System.out.println("Задача 14");
+    }
 }
