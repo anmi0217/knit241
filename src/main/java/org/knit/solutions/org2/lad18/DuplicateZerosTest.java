c96f4ed371276101d7ef80c46c7cb5bfebb89601
nastyaholod <hol05n@mail.ru>
2025-04-19 10:12:03 +0400

    удаление Solution.java - не требуется в проекте добавление тестов для 2.6 лабораторной работы добавила решение 18 задачи выполнила 19 задачу

--- src/main/java/org/knit/solutions/org2/lad18/DuplicateZerosTest.java
+++ src/main/java/org/knit/solutions/org2/lad18/DuplicateZerosTest.java
@@ -1,84 +1,51 @@
 package org.knit.solutions.org2.lad18;
 
 import org.junit.jupiter.api.Test;
-import org.knit.solutions.Solution;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class DuplicateZerosTest {
-
-    @Test
-    void testNormalCases() {
-        Solution solution = new Solution() {
-            @Override
-            public void execute() {
-
-            }
-
-            @Override
-            public void duplicateZeros(int[] arr1) {
-
-            }
-        };
+    @org.junit.jupiter.api.Test
+    public void testNormalCaseses() {
+        DuplicateZeros duplicateZeros = new DuplicateZeros();
 
         int[] arr1 = {1, 0, 2, 3, 0, 4, 5, 0};
-        solution.duplicateZeros(arr1);
+        duplicateZeros.duplicateZeros(arr1);
         assertArrayEquals(new int[]{1, 0, 0, 2, 3, 0, 0, 4}, arr1);
 
         int[] arr2 = {1, 2, 3};
-        solution.duplicateZeros(arr2);
+        duplicateZeros.duplicateZeros(arr2);
         assertArrayEquals(new int[]{1, 2, 3}, arr2);
     }
 
-    @Test
-    void testEdgeCases() {
-        Solution solution = new Solution() {
-            @Override
-            public void execute() {
-
-            }
-
-            @Override
-            public void duplicateZeros(int[] arr1) {
-
-            }
-        };
+    @org.junit.jupiter.api.Test
+    public void testEdgeCases() {
+        DuplicateZeros duplicateZeros = new DuplicateZeros();
 
         int[] emptyArray = {};
-        solution.duplicateZeros(emptyArray);
+        duplicateZeros.duplicateZeros(emptyArray);
         assertArrayEquals(new int[]{}, emptyArray);
 
         int[] allZeros = {0, 0, 0};
-        solution.duplicateZeros(allZeros);
+        duplicateZeros.duplicateZeros(allZeros);
         assertArrayEquals(new int[]{0, 0, 0}, allZeros);
 
         int[] zeroAtEdge = {0, 1, 2, 3, 0};
-        solution.duplicateZeros(zeroAtEdge);
+        duplicateZeros.duplicateZeros(zeroAtEdge);
         assertArrayEquals(new int[]{0, 0, 1, 2, 3}, zeroAtEdge);
     }
 
     @Test
     void testPerformance() {
-        Solution solution = new Solution() {
-            @Override
-            public void execute() {
-
-            }
-
-            @Override
-            public void duplicateZeros(int[] arr1) {
-
-            }
-        };
+        DuplicateZeros duplicateZeros = new DuplicateZeros();
 
         int[] largeArray = new int[10000];
         for (int i = 0; i < largeArray.length; i++) {
             largeArray[i] = i % 2 == 0 ? 0 : 1;
         }
         long startTime = System.currentTimeMillis();
-        solution.duplicateZeros(largeArray);
+        duplicateZeros.duplicateZeros(largeArray);
         long endTime = System.currentTimeMillis();
         System.out.println("Время выполнения: " + (endTime - startTime) + " мс");
     }
 }
-
