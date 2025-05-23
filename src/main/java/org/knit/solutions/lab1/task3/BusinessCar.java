4904f728e54189645ded54899f7cccde1f06b59b
dswsy <114597458+cosmys81@users.noreply.github.com>
2025-05-24 01:52:15 +0400

    Add files via upload

---
+++ src/main/java/org/knit/solutions/lab1/task3/BusinessCar.java
@@ -0,0 +1,8 @@
+package org.knit.solutions.lab1.task3;
+
+public class BusinessCar implements Transport {
+    @Override
+    public String getSpecifications() {
+        return "BusinessCar: Превосходный комфорт и кожа в салоне.";
+    }
+}
---
+++ src/main/java/org/knit/solutions/lab1/task3/FamilyVan.java
@@ -0,0 +1,9 @@
+package org.knit.solutions.lab1.task3;
+
+public class FamilyVan implements Transport {
+    @Override
+    public String getSpecifications() {
+        return "FamilyVan: Вместительный салон для 6 и более человек.";
+    }
+}
+
